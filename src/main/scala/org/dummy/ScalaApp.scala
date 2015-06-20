143a10ae5ef7d7c3454fdc3058abd04489f7b709
Alvaro Agea Herradon <alvaro.agea@gmail.com>
2015-06-20 10:42:52 +0200

    Change spark version

--- src/main/scala/org/dummy/ScalaApp.scala
+++ src/main/scala/org/dummy/ScalaApp.scala
@@ -2,14 +2,13 @@ package org.dummy
 
 import org.apache.spark._
 import org.apache.spark.streaming._
-import org.apache.spark.streaming.StreamingContext._
 
 object ScalaApp extends App {
   val conf = new SparkConf().setMaster("local[2]").setAppName("NetworkWordCount")
   val ssc = new StreamingContext(conf, Seconds(10))
 
 
-  //ATENCION: Antes de arrancar ejecutar el comando  nc -lk 9999
+  //ATENCION: Antes de arrancar ejecutar el comando  it
 
   val lines = ssc.socketTextStream("localhost", 9999)
 
